a36e8f9935f1e67569098025308153c1e55dae5c
Omar Kraidie <omppula_1@hotmail.com>
2022-04-03 20:07:42 +0300

    Initialized basic server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,7 @@
-console.log(`Hello world!`)
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`))
