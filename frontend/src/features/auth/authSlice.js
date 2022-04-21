516769ca7de71ea364b8af5ff145870d230ab5fc
Omar Kraidie <omppula_1@hotmail.com>
2022-04-21 08:25:43 +0300

    Created the auth state with Redux

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+// Get user from localstorage
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+	user: user ? user : null,
+	isError: false,
+	isSuccess: false,
+	isLoading: false,
+	message: '',
+};
+
+// Register user
+
+export const authSlice = createSlice({
+	name: 'auth',
+	initialState,
+	reducers: {
+		reset: (state) => {
+			state.isError = false;
+			state.isSuccess = false;
+			state.isLoading = false;
+			state.message = '';
+		},
+	},
+	extraReducers: () => {},
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
