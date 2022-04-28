a8acb5ee2261d6c49d19690d9eef7af935cec4ee
Omar Kraidie <omppula_1@hotmail.com>
2022-04-28 19:21:22 +0300

    Removed strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,8 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
