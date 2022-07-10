e87cc79d838870691005c83f83eeff3a66b63980
Omar Kraidie <omppula_1@hotmail.com>
2022-07-10 13:10:51 +0300

    scroll only for mobile

--- frontend/src/components/AnnualizedReturnCalculator/AnnualizedReturnReport.jsx
+++ frontend/src/components/AnnualizedReturnCalculator/AnnualizedReturnReport.jsx
@@ -1,19 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { formatCurrency, formatPercentage } from '../../helpers/format';
 
 const AnnualizedReturnReport = ({ report, calculationCount }) => {
+	const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+
 	useEffect(() => {
 		const reportSummary = document.querySelector('.report-container');
 
-		if (reportSummary) {
+		if (windowWidth <= 576 && reportSummary) {
 			window.scrollTo({
-				top: reportSummary.offsetTop - 450,
+				top: reportSummary.offsetTop - 60,
 				left: 0,
 				behavior: 'smooth',
 			});
 		}
 	}, [calculationCount]);
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowWidth(getWindowWidth());
+		}
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	function getWindowWidth() {
+		const { innerWidth: width } = window;
+		return width;
+	}
 
 	const { startingBalance, endingBalance, annualizedReturn, percentReturn, currency } = report;
 
