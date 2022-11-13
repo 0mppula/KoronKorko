84a9137d629a6998d3316a920010182c9f14575c
Omar Kraidie <omppula_1@hotmail.com>
2022-11-13 18:57:23 +0200

    clean up

--- frontend/src/components/Tools/RedirectFromRoot.js
+++ frontend/src/components/Tools/RedirectFromRoot.js
@@ -1,18 +1,18 @@
 import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const RedirectFromRoot = () => {
 	const { pathname } = useLocation();
-	const navigate = useNavigate();
 	const url = window.location.href;
 
 	useEffect(() => {
 		// If in root url redirect to www.koronkorko.com
 		const devUrl = window.location.href === 'http://localhost:3000/';
 		const herokuUrl = window.location.href === 'https://koronkorko.herokuapp.com/';
+		const suffix = window.location.href.split(/\//);
 
 		if (!String(url).match(/\/\/www./) && !devUrl && !herokuUrl) {
-			window.location.href = 'http://www.koronkorko.com/';
+			window.location.href = `http://www.koronkorko.com/${suffix}`;
 		}
 	}, [pathname, url]);
 
