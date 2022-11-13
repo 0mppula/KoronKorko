0d66a6620e63310d9a6cddcc5f13798ae8a87abb
Omar Kraidie <omppula_1@hotmail.com>
2022-11-13 19:16:20 +0200

    cleaned up suffix

--- frontend/src/components/Tools/RedirectFromRoot.js
+++ frontend/src/components/Tools/RedirectFromRoot.js
@@ -7,9 +7,9 @@ const RedirectFromRoot = () => {
 
 	useEffect(() => {
 		// If in root url redirect to www.koronkorko.com
-		const devUrl = window.location.href === 'http://localhost:3000/';
-		const herokuUrl = window.location.href === 'https://koronkorko.herokuapp.com/';
-		const suffix = window.location.href.split(/\//);
+		const devUrl = window.location.href?.startsWith('http://localhost:3000/');
+		const herokuUrl = window.location.href?.startsWith('https://koronkorko.herokuapp.com/');
+		const suffix = window.location.href.split(/.com\//);
 
 		if (!String(url).match(/\/\/www./) && !devUrl && !herokuUrl) {
 			window.location.href = `http://www.koronkorko.com/${suffix[suffix.length - 1]}`;
