869c0faac759565ad4c120368f43d24713a5a49b
Omar Kraidie <omppula_1@hotmail.com>
2023-01-05 10:39:22 +0200

    Cleanup

--- frontend/src/helpers/checkKeyDown.js
+++ frontend/src/helpers/checkKeyDown.js
@@ -1,11 +1,11 @@
 const checkKeyDown = (e, fn) => {
 	// Call the passed function only when "Enter" or "Space" keys are pressed
 	let keycode = e.keyCode || e.which;
-	if (keycode == '32') {
+	if (keycode === 32) {
 		// Prevent scroll on when pressing the "Space Key"
 		e.preventDefault();
 		fn();
-	} else if (keycode == '13') {
+	} else if (keycode === 13) {
 		fn();
 	}
 };
