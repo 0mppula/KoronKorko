4832a492d5cf98a8a66fbb0678b1526672d978c2
Omar Kraidié <omppula_1@hotmail.com>
2023-04-16 13:01:53 +0300

    Updated scroll logic.

--- frontend/src/hooks/useScrollToReportContainer.js
+++ frontend/src/hooks/useScrollToReportContainer.js
@@ -1,20 +1,24 @@
 import { useEffect } from 'react';
+import { useWindowWidth } from './useWindowWidth';
 
 /**
  *
- * If the top of the report summary container is not sufficiantly visible, this hook will scroll to
- * the top of the container.
+ * If the top of the report summary container is not sufficiantly visible or the app is viewed with
+ * a mobile device, this hook will scroll to the top of the report summary container.
  * @param {number} calculationCount The count of the number of calculations made. Every calculation
  * button click should increments this variable.
  */
 export const useScrollToReportContainer = (calculationCount) => {
+	const windowWidth = useWindowWidth();
+
 	useEffect(() => {
 		const reportSummary = document.querySelector('.report-container');
 		const reportSummaryDimensions = reportSummary.getBoundingClientRect();
 		// Atleast 60px should be visible from the top of the element.
 		const topOfReportSummaryIsVisible = reportSummaryDimensions.top + 60 < window.innerHeight;
+		const isMobile = windowWidth <= 576;
 
-		if (!topOfReportSummaryIsVisible && reportSummary) {
+		if ((!topOfReportSummaryIsVisible || isMobile) && reportSummary) {
 			window.scrollTo({
 				// Take into account the height of the nav and some scroll padding to the top.
 				top: reportSummaryDimensions.top - 60 + window.pageYOffset,
