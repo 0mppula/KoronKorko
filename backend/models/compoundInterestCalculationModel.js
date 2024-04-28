4d0a871e0996cdbbe9df7f131607a79cbe721d94
Omar Kraidié <omppula_1@hotmail.com>
2024-04-28 13:36:23 +0300

    Fixed compound interest calculation schema validation error.

--- backend/models/compoundInterestCalculationModel.js
+++ backend/models/compoundInterestCalculationModel.js
@@ -10,13 +10,13 @@ const compoundInterestCalculationSchema = mongoose.Schema(
 		name: {
 			type: String,
 			required: [true, 'Please name your calculation'],
-			maxlength: [30, 'The maximum character length for names is 30']
+			maxlength: [30, 'The maximum character length for names is 30'],
 		},
 		formData: {
 			compoundFrequency: { value: { type: Number }, label: { type: String } },
 			contribution: { type: Number, min: 0 },
 			contributionFrequency: { value: { type: Number }, label: { type: String } },
-			contributionMultiplier: { type: Number, min: 0 },
+			contributionMultiplier: { type: Number, min: -1, max: 1 },
 			duration: { type: Number, min: 0 },
 			durationMultiplier: { value: { type: Number }, label: { type: String } },
 			interestRate: { type: Number, min: 0 },
